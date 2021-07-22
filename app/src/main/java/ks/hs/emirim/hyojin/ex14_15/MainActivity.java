ddb6cf95e292e0507e2cf5b04f3c0e561d23dec0
User <w2030@e-mirim.hs.kr>
2021-07-22 15:30:12 +0900

    Edit main_activity(single choiceitems) Commit

--- app/src/main/java/ks/hs/emirim/hyojin/ex14_15/MainActivity.java
+++ app/src/main/java/ks/hs/emirim/hyojin/ex14_15/MainActivity.java
@@ -7,36 +7,56 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    String [] fruitsArr = {"복숭아", "체리", "딸기"};
+    int[] imgsArr = {R.drawable.i01, R.drawable.i02, R.drawable.i03};
+    Button btnFruits;
+    ImageView imgV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnFruits = findViewById(R.id.btn_fruits);
         Button btnDialog = findViewById(R.id.btn_dialog);
         btnDialog.setOnClickListener(btnListener);
+        btnFruits.setOnClickListener(btnListener);
+        imgV = findViewById(R.id.imgv);
     }
 
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-//            AlertDialog.Builder dialog = new AlertDialog.Builder(getApplicationContext());
-//            dialog.setTitle(R.string.dialog_title);
-//            dialog.setMessage(R.string.dialog_message);
-//            dialog.setIcon(R.drawable.warning);
-//            dialog.setPositiveButton(R.string.btn_positive, btnPositiveListener);
-//            dialog.show();
-
-            new AlertDialog.Builder(MainActivity.this)
-            .setTitle(R.string.dialog_title)
-            .setMessage(R.string.dialog_message)
-            .setIcon(R.drawable.warning)
-            .setPositiveButton(R.string.btn_positive, btnPositiveListener)
-            .show();
+            switch (v.getId()){
+                case R.id.btn_dialog:
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle(R.string.dialog_title)
+                            .setSingleChoiceItems(fruitsArr,0,fruitsItemListener)
+                            .setIcon(R.drawable.warning)
+                            .setPositiveButton(R.string.btn_positive, btnPositiveListener)
+                            .show();
+                    break;
+                case R.id.btn_fruits:
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle(R.string.btn_fruits)
+                            .setItems(fruitsArr, fruitsItemListener)
+                            .setIcon(R.drawable.peach)
+                            .setPositiveButton("닫기",null)
+                            .show();
+                    break;
+            }
         }
     };
+    DialogInterface.OnClickListener fruitsItemListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            btnFruits.setText(fruitsArr[which]);
+            imgV.setImageResource(imgsArr[which]);
+        }
+    };
+
     DialogInterface.OnClickListener btnPositiveListener = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialog, int which) {
